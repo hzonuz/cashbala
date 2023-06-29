0407c5f8f33aa4fcf826cc45a3787ac353a1ce9e
Hasan Zonouzi <hasanzon@yahoo.com>
2023-06-29 16:56:41 +0330

    feat: test signup api

--- server/users/tests.py
+++ server/users/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class TestSignUp(TestCase):
+    def test_signup(self):
+        response = self.client.post(
+            "/users/signup/v0/",
+            {
+                "username": "testuser",
+                "password": "Test1234!",
+                "email": "test@gmail.com",
+                "first_name": "Test",
+                "last_name": "User"
+            },
+            content_type="application/json",
+        )
+        print(response.data)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data["username"], "testuser")
+        self.assertEqual(response.data["email"], "test@gmail.com")
+        self.assertEqual(response.data["first_name"], "Test")
+        self.assertEqual(response.data["last_name"], "User")
